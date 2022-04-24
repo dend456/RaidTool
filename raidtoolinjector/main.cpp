64d3ce0ea103cb336f20bddf30cd9af8b8d18809
nah <pokports@gmail.com>
2022-04-24 14:19:25 -0400

    use character name for choosing process

--- raidtoolinjector/main.cpp
+++ raidtoolinjector/main.cpp
@@ -5,13 +5,84 @@
 #include <vector>
 #include <VersionHelpers.h>
 #include <atlstr.h>
+#include <filesystem>
+#include <fmt/core.h>
+#include <tlhelp32.h>
+
+uint64_t charAddr = 0;
+uint64_t nameOffset = 0;
 
 struct EQProc
 {
     int pid;
     SYSTEMTIME time;
-    EQProc(int pid, SYSTEMTIME time) : pid(pid), time(time) {}
+    std::string character;
+    EQProc(int pid, SYSTEMTIME time, std::string character) : pid(pid), time(time), character(character) {}
 };
+
+bool loadOffsets(const std::filesystem::path& path) noexcept
+{
+    if (!std::filesystem::exists(path)) return false;
+
+    try
+    {
+        char str[255] = { 0 };
+        std::string pathStrtmp = path.string();
+        const char* pathStr = pathStrtmp.c_str();
+
+        GetPrivateProfileStringA("EQ", "charAddr", TEXT(""), str, sizeof(str), pathStr);
+        charAddr = std::stoi(std::string(str), 0, 0);
+        GetPrivateProfileStringA("SpawnInfo", "nameOffset", TEXT(""), str, sizeof(str), pathStr);
+        nameOffset = std::stoi(std::string(str), 0, 0);
+    }
+    catch (const std::exception& e)
+    {
+        fmt::print("Error loading ini: {}\n", std::string(e.what()));
+        return false;
+    }
+    return true;
+}
+
+uint64_t getBaseAddress(DWORD pid)
+{
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+    auto e = GetLastError();
+    MODULEENTRY32 me = { 0 };
+    me.dwSize = sizeof(me);
+    auto ret = Module32First(snapshot, &me);
+    if (!ret) return 0;
+    while (ret)
+    {
+        if (strcmp(me.szModule, "eqgame.exe") == 0)
+        {
+            CloseHandle(snapshot);
+            return (uint64_t)me.hModule;
+        }
+        ret = Module32Next(snapshot, &me);
+    }
+    CloseHandle(snapshot);
+    return 0;
+}
+
+std::string getCharacter(HANDLE handle, DWORD pid)
+{
+    SIZE_T read;
+    uint64_t baseAddr = getBaseAddress(pid);
+    if (!baseAddr) return "";
+
+    uint64_t nameAddr = 0;
+    if (ReadProcessMemory(handle, (LPCVOID)(baseAddr + charAddr), &nameAddr, sizeof(nameAddr), &read))
+    {
+        nameAddr += nameOffset;
+        char name[16] = { 0 };
+        if (ReadProcessMemory(handle, (LPCVOID)(nameAddr), name, sizeof(name), &read))
+        {
+            return name;
+        }
+    }
+    return "";
+}
+
 
 BOOL InjectDLL(DWORD ProcessID)
 {
@@ -70,21 +141,26 @@ BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam) {
     {
         FILETIME creation, exit, kernal, user;
         SYSTEMTIME sysTime;
-        HANDLE handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, dwProcessId);
+        HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, 1, dwProcessId);
         if (GetProcessTimes(handle, &creation, &exit, &kernal, &user) == 0)
         {
             printf("Error getting times for pid %d\n", dwProcessId);
             CloseHandle(handle);
             return true;
         }
+
+        std::string character = getCharacter(handle, dwProcessId);
+
         CloseHandle(handle);
         FileTimeToSystemTime(&creation, &sysTime);
-        procs.emplace_back(dwProcessId, sysTime);
+        procs.emplace_back(dwProcessId, sysTime, character);
     }
     return true;
 }
 
-int main() {
+int main() 
+{
+    loadOffsets(std::filesystem::current_path() / "offsets.ini");
     EnumWindows(EnumWindowsProc, NULL);
     DWORD pid = 0;
     if (procs.size() == 0)
@@ -97,12 +173,21 @@ int main() {
     }
     else
     {
-        for (const auto& p : procs)
+        /*for (const auto& p : procs)
         {
-            printf("pid: %d\tstarted: %02d:%02d:%02d\n", p.pid, p.time.wHour, p.time.wMinute, p.time.wSecond);
+            printf("pid: %d\tstarted: %02d:%02d:%02d\t%s\n", p.pid, p.time.wHour, p.time.wMinute, p.time.wSecond, p.character.c_str());
+        }*/
+        for (int i = 0; i < procs.size(); ++i)
+        {
+            fmt::print("{}: {}\n", i, procs[i].character);
         }
-        std::cout << "Pick Target ProcessID\n";
-        std::cin >> pid;
+        std::cout << "Select EQ instance: ";
+        int selection = -1;
+        std::cin >> selection;
+        if (selection >= 0 && selection < procs.size())
+        {
+            pid = procs[selection].pid;
+        }
     }
     if (pid)
     {
